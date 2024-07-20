35477fcf2abbf2a0019b000bfe169a9feed43279
Ubuntu <ubuntu@ip-172-31-10-128.ap-south-1.compute.internal>
2024-07-20 12:49:51 +0000

    update

--- db.py
+++ db.py
@@ -9,5 +9,4 @@ class MongoDB:
     def get_collection(self, collection_name):
         return self.db[collection_name]
 
-
 mongo_db = MongoDB("mongodb+srv://aniket:12345@cluster0.8sfpess.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
